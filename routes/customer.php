5fc92cfa7cc1d27ad87df600c08dbca6607d057f
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-06-05 05:12:07 -0300

    tests: filtering month and year

--- routes/customer.php
+++ routes/customer.php
@@ -5,6 +5,6 @@ use App\Applications\Api\Customer\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(TransactionController::class)->prefix('transactions')->group(function () {
-    Route::get('', 'index');
+    Route::any('', 'index');
 });
 
--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -18,19 +18,38 @@ class TransactionTest extends TestCase
         parent::setUp();
         $this->base_route = 'api/customer/transactions/';
     }
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_should_list_all_transactions()
+
+    public function test_field_year_should_be_required_on_list(){
+        $response = $this->get($this->base_route, [
+            'month' => '1'
+        ]);
+        $response->assertStatus(422)
+            ->assertJson([
+                "year" => ["The year field is required."]
+            ]);
+    }
+
+    public function test_field_month_should_be_required_on_list(){
+        $response = $this->get($this->base_route, [
+            'year' => date('Y')
+        ]);
+        $response->assertStatus(422)
+            ->assertJson([
+                "month" => ["The month field is required."]
+            ]);
+    }
+
+    public function test_should_list_all_transactions(): void
     {
         $user = User::factory()
             ->has(Transaction::factory()->count(2))
             ->create();
         $this->actingAs($user);
 
-        $response = $this->get($this->base_route);
+        $response = $this->post($this->base_route, [
+            'year' => date('Y'),
+            'month' => date('m')
+        ]);
         $response->assertStatus(200);
         $this->assertCount(2, $response->json());
     }
