95b22ab8ba68aa36bd8ec917377e6e0490ee85e3
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-06-05 22:46:11 -0300

    test: required inputs

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -30,4 +30,27 @@ class AuthenticationTest extends TestCase
             ]);
     }
 
+    public function test_field_email_should_be_required_to_register_new_user(){
+        $mock_data = [
+            "username" => "mock_username",
+            "password" => "mock_password"
+        ];
+        $response = $this->post($this->base_route . "register", $mock_data);
+        $response->assertStatus(422)
+            ->assertJson([
+                "email" => ["The email field is required."]
+            ]);
+    }
+
+    public function test_password_password_should_be_required_to_register_new_user(){
+        $mock_data = [
+            "username" => "mock_username",
+            "email" => "mockmail@gmail.com"
+        ];
+        $response = $this->post($this->base_route . "register", $mock_data);
+        $response->assertStatus(422)
+            ->assertJson([
+                "password" => ["The password field is required."]
+            ]);
+    }
 }
